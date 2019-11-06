bf72e6ef6044c0d12431f20fb8161b8255a5c577
Stefan Walter <stefan.walter@inf.ethz.ch>
2019-11-06 16:00:27 +0100

    Undo previous change for multiple header file support for nfs-utils

--- regex.c
+++ regex.c
@@ -45,12 +45,7 @@
 #include <ini_configobj.h>
 
 #include "nfsidmap.h"
-#ifdef HAVE_NFSIDMAP_INTERNAL_H
 #include "nfsidmap_internal.h"
-#endif
-#ifdef HAVE_NFSIDMAP_PLUGIN_H
-#include "nfsidmap_plugin.h"
-#endif
 
 #define MAX_MATCHES 100
 
